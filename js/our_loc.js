46ad2269de01cc29b60c40a560ec26da9aee9ab1
Andy Zhou <44891657+z-t-y@users.noreply.github.com>
2020-01-24 15:05:36 +0800

    Update to the Accurate Location

--- js/our_loc.js
+++ js/our_loc.js
@@ -2,7 +2,7 @@ window.onload = showMap
 function showMap() {
     var map = new BMap.Map("map");
     map.disableBizAuthLogo();
-    var point = new BMap.Point(121.532988, 31.32133);
+    var point = new BMap.Point(121.533154, 31.32097);
     map.centerAndZoom(point, 15);
     map.enableScrollWheelZoom(true);
     var marker = new BMap.Marker(point);
