806a6c0e11555ebaf530a665df0722ce4e9060db
Andy Zhou <44891657+z-t-y@users.noreply.github.com>
2020-01-26 10:27:40 +0800

    Add auto_load_nav.js and A Video to the Website

---
+++ js/auto_load_nav.js
@@ -0,0 +1,25 @@
+function add_nav() {
+    document.write(`
+    <nav>
+        <ul>
+            <li><a href="https://z-t-y.github.io/LSFDClass202201">主页</a></li>
+            <li><a href="https://z-t-y.github.io/LSFDClass202201/classmates.html">
+                    班级同学
+                </a></li>
+            <li><a href="https://z-t-y.github.io/LSFDClass202201/our_mv.html"
+                我们的MV
+            </a></li>
+            <li><a href="https://z-t-y.github.io/LSFDClass202201/wishes.html">
+                    我们的愿景
+                </a></li>
+            <li><a href="https://z-t-y.github.io/LSFDClass202201/cat_pics.html">
+                    众生猫猫相
+                </a></li>
+            <li><a href="location.html">
+                我们在哪
+            </a></li>
+        </ul>
+    </nav>
+    `)
+}
+add_nav()
